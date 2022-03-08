21f620e85ad29bfc89866300f14ba8a7103bad48
Brian Sohn <kolpp15@gmail.com>
2022-03-07 16:25:41 -0800

    update event listener

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+const starWrapper = document.querySelector(".stars");
 const stars = document.querySelectorAll(".stars a");
 
-
-stars.forEach((star, idx) => {
+stars.forEach((star, clickedIdx) => {
   star.addEventListener("click", () => {
-    console.log(`star ${idx} was clicked`);
+    starWrapper.classList.add("disabled");
+    stars.forEach((otherStar, otherIdx) => {
+      if (otherIdx <= clickedIdx) {
+        otherStar.classList.add("active");
+      }
+    });
+    console.log(`star of index ${clickedIdx + 1} was clicked`);
   });
 });
-
